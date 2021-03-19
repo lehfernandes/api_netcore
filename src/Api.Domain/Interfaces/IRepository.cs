3ecbd7dd395ea2d62d6783f647fe8c50a74703fb
lehfernandes <leandrofernandes016@gmail.com>
2021-03-18 22:33:05 -0300

    Add ExsitAsync in IRepository

--- src/Api.Domain/Interfaces/IRepository.cs
+++ src/Api.Domain/Interfaces/IRepository.cs
@@ -10,6 +10,7 @@ namespace Api.Domain.Interfaces
          Task<T> InsertAsync(T item);
          Task<T> UpdateAsync(T item);
          Task<bool> DeleteAsync(Guid id);
+         Task<bool> ExistAsync (Guid id);
          Task<T> SelectAsync(Guid id);
          Task<IEnumerable<T>> SelectAsync();
     }

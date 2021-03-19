299740e86ba4531e06578a7e712013d4f2807a34
lehfernandes <leandrofernandes016@gmail.com>
2021-03-19 02:11:22 -0300

    Add injection

---
+++ src/Api.CrossCutting/DependecyInjection/ConfigureService.cs
@@ -0,0 +1,15 @@
+using Api.Domain.Interfaces.Services.User;
+using Api.Service.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.CrossCutting.DependecyInjection
+{
+    public class ConfigureService
+    {
+        public static void ConfigureDependeciesService(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IUserService, UserService>();
+            
+        }
+    }
+}

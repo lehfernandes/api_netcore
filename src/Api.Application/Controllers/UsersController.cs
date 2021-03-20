ceb5fe7e43e00d5e023047b71ef38ad12e22ed04
lehfernandes <leandrofernandes016@gmail.com>
2021-03-20 00:43:12 -0300

    Add metod GetAll and Post

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,10 +20,9 @@ namespace Api.Application.Controllers
         [HttpGet]
         public async Task<ActionResult> GetAll()
         {
-            if(!ModelState.IsValid)
-            {
+            if(!ModelState.IsValid)            
                 return BadRequest(ModelState);
-            }
+            
 
             try
             {
@@ -34,5 +34,45 @@ namespace Api.Application.Controllers
             }
             
         }
-    }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetWithId")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+
+            if(!ModelState.IsValid)                        
+                return BadRequest(ModelState);
+
+            try
+            {
+                return Ok(await _service.Get(id));
+            }
+            catch (ArgumentException ex)
+            {                
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }                        
+            
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post ([FromBody] UserEntity user)
+        {
+            if(!ModelState.IsValid)                        
+                return BadRequest(ModelState);
+            
+            try
+            {
+                var result = await _service.Post(user);
+                if(result != null)
+                    return Created(new Uri(Url.Link("GetWithId", new {id = result.Id})), result);
+                else
+                    return BadRequest();
+            }
+            catch (ArgumentException ex)
+            {                
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }     
+        }
+
+    }    
 }
